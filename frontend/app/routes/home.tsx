275c1996536bab6842bc26d862946d58beae684f
Grant Lancaster <grant.lancaster3@gmail.com>
2025-05-07 08:44:37 -0600

    Shifting to a new site build
    I've wanted to make a clicker game for a while now, so I am transitioning this project to that. So far, changed the post area to the main game play square, and setup some basic layouts for inventory and stats panels to live on the home screen as well.

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,16 +1,18 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import NavBar from "~/components/navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Gear Box" },
+    { name: "description", content: "An idle clicker loot game" },
   ];
 }
 
 export default function Home() {
   return (
   <>
+    <NavBar />
     <Welcome />
   </>
   )
--- frontend/app/welcome/welcome.tsx
+++ frontend/app/welcome/welcome.tsx
@@ -4,17 +4,28 @@ import Post from "~/components/post";
 
 export function Welcome() {
   return (
-    <main className="flex justify-center pt-16 pb-4 max-w-[1440px] bg-slate-900">
-      <div className="flex-1 flex flex-col items-center gap-16">
+    <>
+      <div className="flex gap-4 justify-center items-center">
 
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <div className="bg-red-500 w-[200px] h-[400px]">
+        {/* This will be the stats panel on the left side */}
+      </div>
 
+      <main className="flex justify-center mt-16 w-1/2 max-w-[1440px] bg-slate-900">
+        <div className="flex-1 flex flex-col items-center gap-16">
+
+        <Post />
+
+        </div>
+      </main>
+
+      <div className="bg-red-500 w-[200px] h-[400px]">
+        {/* This will be the inventory on the right side */}
+      </div>
 
       </div>
-    </main>
+
+    </>
   );
 }
 
